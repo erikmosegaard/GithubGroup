f93a745436d66a1ebb90ecb02ff688d736dcee47
erikmosegaard <erikmosegaard28@gmail.com>
2019-02-26 11:21:47 +0100

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,50 @@
+﻿
+
+
+
+
+#vmLS3
+mkdir  Backup-Restore
+
+ls
+#Ausgabe
+#Backup-Restore
+
+cd Backup-Restore
+#Dir change
+
+mkdir Marti_AG
+
+ls
+#Ausgabe
+#Marti_AG
+
+
+cd Marti_AG
+#Dir change
+
+mkdir Chef
+mkdir user1
+mkdir user2
+mkdir user3
+mkdir user4
+mkdir user5
+mkdir user6
+mkdir user7
+mkdir user8
+mkdir user9
+
+ls
+#Ausgabe
+<#
+Chef
+user1
+user2
+user3
+user4
+user5
+user6
+user7
+user8
+user9
+#>
